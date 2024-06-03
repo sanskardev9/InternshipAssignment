73621fc2d0bc0627a8507dd8abd5ff88f08119d1
sanskardev9 <162478615+sanskardev9@users.noreply.github.com>
2024-06-03 11:46:39 +0530

    added the hover effect and read me button to new tab

--- src/app.js
+++ src/app.js
@@ -1,18 +1,68 @@
-  var modal = document.getElementById("myModal");
-
-  var btn = document.getElementById("openModalBtn");
-
-  var span = document.getElementsByClassName("close")[0];
-
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
+  document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("openModalBtn");
+    var span = document.getElementsByClassName("close")[0];
+  
+  
+    // document.getElementById("readMoreBtn").addEventListener("click",function() {
+    //   window.location.href = "https://www.fylehq.com/";
+    //   console.log("clicked");
+    // })
+    
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+  
+    span.onclick = function() {
       modal.style.display = "none";
     }
-  }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  
+    $(document).ready(function() {
+      var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 5
+          }
+        }
+      });
+
+      $('.custom-dots .dot').click(function() {
+        var slideIndex = $(this).data('slide');
+        var owlItems = owl.find('.owl-item').not('.cloned').length;
+
+        owl.trigger('to.owl.carousel', [(slideIndex - 1) * itemsPerGroup, 300]);
+  
+        $('.custom-dots .dot').removeClass('active');
+        $(this).addClass('active');
+      });
+  
+      $('.custom-dots .dot').first().addClass('active');
+
+      owl.on('changed.owl.carousel', function(event) {
+        var owlItems = owl.find('.owl-item').not('.cloned').length;
+        var itemsPerGroup = Math.ceil(owlItems / 3); 
+        var index = Math.floor((event.item.index % owlItems) / itemsPerGroup) + 1;
+  
+        $('.custom-dots .dot').removeClass('active');
+        $('.custom-dots .dot[data-slide="' + index + '"]').addClass('active');
+      });
+    });
+  });
+  
